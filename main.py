df59f1766e3cbc7d5cdca610a8ac90917b0a5720
Daniel Phillips <daniel.j.phillips@gmail.com>
2018-02-09 11:07:00 -0800

    update comments

--- main.py
+++ main.py
@@ -1,31 +1,36 @@
 from flask import Flask, request
 
+#Create the app object from the Flask constructor
 app = Flask(__name__)
+#the DEBUG configuration setting for the Flask application will be enabled. This enables some behaviors that are helpful when developing Flask apps, such as displaying errors in the browser, and ensuring file changes are reloaded while the server is running (aka "host swapping")
 app.config['DEBUG'] = True
 
-#get    <form action="/hello">
+#form string in main.py for now, will put into a template later
 form = """
 <!doctype html>
 <html>
-  <body>
-    <form action="/hello" method="post">
-      <label for="first-name">First Name:</label>
-      <input id="first-name" type="text" name="first_name" />
-      <input type="submit" />
-    </form>
-  </body>
+    <body>
+        <form action="/hello" method="post">
+            <label for="first-name">First Name:</label>
+            <input id="first-name" type="text" name="first_name" />
+            <input type="submit" />
+        </form>
+    </body>
 </html>
 """
 
+#decorator that maps root path "/" to function
 @app.route("/")
 def index():
     return form
 
-#@app.route("/hello")
+#a new route to handle the form submission as a post request
 @app.route("/hello", methods=['POST'])
 def hello():
-#    first_name = request.args.get('first_name')
+    #retrieve the data from the form object (like a dictionary)
     first_name = request.form['first_name']
-    return "<h1>Hello, " + first_name + "</h1>"
+    #return the HTML for the browser to render
+    return '<h1>Hello, ' + first_name + '</h1>'
 
+#run the app
 app.run()
