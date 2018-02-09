9d962d4e4248d9d3e8f42f8d2248b83283564ee3
Daniel Phillips <daniel.j.phillips@gmail.com>
2018-02-09 11:36:16 -0800

    Validate time form and handler

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from flask import Flask, request
+from flask import Flask, request, redirect
+
 
 #Create the app object from the Flask constructor
 app = Flask(__name__)
 #the DEBUG configuration setting for the Flask application will be enabled. This enables some behaviors that are helpful when developing Flask apps, such as displaying errors in the browser, and ensuring file changes are reloaded while the server is running (aka "host swapping")
 app.config['DEBUG'] = True
+
 
 #form string in main.py for now, will put into a template later
 form = """
@@ -19,10 +21,12 @@ form = """
 </html>
 """
 
+
 #decorator that maps root path "/" to function
 @app.route("/")
 def index():
     return form
+
 
 #a new route to handle the form submission as a post request
 @app.route("/hello", methods=['POST'])
@@ -31,6 +35,83 @@ def hello():
     first_name = request.form['first_name']
     #return the HTML for the browser to render
     return '<h1>Hello, ' + first_name + '</h1>'
+
+
+time_form = """
+    <style>
+        .error {{ color: red; }}
+    </style>
+    <h1>Validate Time</h1>
+    <form method='POST'>
+        <label>Hours (24-hour format)
+            <input name="hours" type="text" value='{hours}' />
+        </label>
+        <p class="error">{hours_error}</p>
+        <label>Minutes
+            <input name="minutes" type="text" value='{minutes}' />
+        </label>
+        <p class="error">{minutes_error}</p>
+        <input type="submit" value="Validate" />
+    </form>
+    """
+
+
+@app.route('/validate-time')
+def display_time_form():
+    return time_form.format(hours='', hours_error='',
+        minutes='', minutes_error='')
+
+
+def is_integer(num):
+    try:
+        int(num)
+        return True
+    except ValueError:
+        return False
+
+
+@app.route('/validate-time', methods=['POST'])
+def validate_time():
+
+    hours = request.form['hours']
+    minutes = request.form['minutes']
+
+    hours_error = ''
+    minutes_error = ''
+
+    if not is_integer(hours):
+        hours_error = 'Not a valid integer'
+        hours = ''
+    else:
+        hours = int(hours)
+        if hours > 23 or hours < 0:
+            hours_error = 'Hour value out of range (0-23)'
+            hours = ''
+
+    if not is_integer(minutes):
+        minutes_error = 'Not a valid integer'
+        minutes = ''
+    else:
+        minutes = int(minutes)
+        if minutes > 59 or minutes < 0:
+            minutes_error = 'Minutes value out of range (0-59)'
+            minutes = ''
+
+    if not minutes_error and not hours_error:
+        time = str(hours) + ':' + str(minutes)
+        return redirect('/valid-time?time={0}'.format(time))
+    else:
+        return time_form.format(hours_error=hours_error,
+            minutes_error=minutes_error,
+            hours=hours,
+            minutes=minutes)
+
+
+@app.route('/valid-time')
+def valid_time():
+    time = request.args.get('time')
+    return '<h1>You submitted {0}. Thanks for submitting a valid time!</h1>'.format(time)
+
 
 #run the app
 app.run()
